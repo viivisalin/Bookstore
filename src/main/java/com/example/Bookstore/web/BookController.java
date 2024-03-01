cb796e0d9286575cafd36e78875216deff150075
viivisalin <salin.viivi@gmail.com>
2024-03-01 15:09:06 +0200

    Exercise 2 booklist.html created

--- src/main/java/com/example/Bookstore/web/BookController.java
+++ src/main/java/com/example/Bookstore/web/BookController.java
@@ -1,16 +1,22 @@
 package com.example.Bookstore.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.example.Bookstore.model.BookRepository;
+
 @Controller
 public class BookController {
+
+    @Autowired
+    BookRepository bookRepository;
     
-    @RequestMapping(value="/index", method=RequestMethod.GET)
+    @RequestMapping(value="/books", method=RequestMethod.GET)
     public String showIndex(Model model) {
-    	System.out.println("Index endpoint");
-        return "index";
+    	System.out.println("Books");
+        return "books";
     }
 }
