3c903b0ecc7ba818839fe146c5939d0e17de4d2f
viivisalin <salin.viivi@gmail.com>
2024-03-03 14:59:05 +0200

    Chapter 4 exercise 1 done

--- src/main/java/com/example/Bookstore/model/BookRepository.java
+++ src/main/java/com/example/Bookstore/model/BookRepository.java
@@ -2,6 +2,7 @@ package com.example.Bookstore.model;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface BookRepository extends CrudRepository<Book, Long>{
 
+public interface BookRepository extends CrudRepository<Book, Long>{
+    // Book findByTitle(String title);
 }
--- src/main/java/com/example/Bookstore/model/CategoryRepository.java
+++ src/main/java/com/example/Bookstore/model/CategoryRepository.java
@@ -1,5 +1,9 @@
 package com.example.Bookstore.model;
 
-public interface CategoryRepository {
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface CategoryRepository extends CrudRepository<Category, Long>{
+  //  Category findByName(String name);
 
 }
