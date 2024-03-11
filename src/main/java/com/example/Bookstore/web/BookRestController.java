d80cb937ca4cb4078fd77819c4447536e3ea2cd5
viivisalin <salin.viivi@gmail.com>
2024-03-11 12:31:26 +0200

    WebSecurityConfig class added

--- src/main/java/com/example/Bookstore/web/BookRestController.java
+++ src/main/java/com/example/Bookstore/web/BookRestController.java
@@ -3,7 +3,7 @@ package com.example.Bookstore.web;
 import java.util.List;
 import java.util.Optional;
 
-import org.aspectj.internal.lang.annotation.ajcDeclareAnnotation;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 
